1962111a5fcd073b5f3437e494c428c2887f326a
Raul Gallegos <raul.ogh@gmail.com>
2018-03-31 22:12:23 -0500

    ctci: adding 1.1 exercise

---
+++ tests/ctci/exercise1_1.py
@@ -0,0 +1,17 @@
+"""
+Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+"""
+def unique_characters(string):
+    a = {}
+    for c in string:
+        if c in a:
+            return False
+        a[c] = True
+
+
+def test_unique_characters():
+    repeated_chars = 'aaaaaaaaaa'
+    unique_chars = '1234567890-=qwertyuiop[]asdfghjkl;zxcvbnm,./'
+
+    assert not unique_characters(repeated_chars)
+    assert unique_chars
